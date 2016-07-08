471548e63f9a049e7f3fdc492501b1d30ea999d1
dandapataishwarya <dandapataishwarya@gmail.com>
2016-07-08 14:55:37 +0530

    finish

---
+++ README.c
@@ -0,0 +1,16 @@
+# alpha
+#include<stdio.h>
+voiod main()
+{
+char alpha;
+printf("Enter a Alphabet");
+scanf("%c",&alpha);
+if((alpha>='a'&&alpha='z')||(alpha>='A'&&alpha>='Z'))
+{
+ printf("character is alphabet");
+ }
+ else
+ {
+  printf("Not an alphabet");
+  }
+  }
